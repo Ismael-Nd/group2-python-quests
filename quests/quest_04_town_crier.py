543720f8b0df907c51fb71f43a5dba190c742746
macharia782 <j.macharia@alustudent.com>
2026-06-29 01:46:52 +0300

    added town crier quest

---
+++ quests/quest_04_town_crier.py
@@ -0,0 +1,10 @@
+#Quest 4: The Town Crier
+#Concept: Combining variables and strings for output.
+#How to use it: print("Hello, " + hero_name) or f-strings: print(f"Hello, {hero_name}")
+#Why it's important: You'll almost always be displaying information stored in your variables.
+#Logical Reasoning: To create a dynamic message, I need to combine my fixed text with the values stored in my variables.
+#The Quest: Create variables for a city name, the current year, and your name. Print a message like: "Welcome to [City Name]! The year is [Year], and our newest resident is [Your Name]."
+city_name = "Kigali"
+current_year = "2023"
+newest_resident = "Mach"
+print(f"Welcome to {city_name}! The year is {current_year}, and our newest resident is {newest_resident}.")
