db66c9a514d8c66c539ac55197b9b5bd9db2b669
hahmed2-crypto <h.ahmed2@lustudent.com>
2026-06-29 01:51:49 -0700

    Question 15

---
+++ quests/quest_15_nested_riddle.py
@@ -0,0 +1,10 @@
+#!/bin/bash/python3
+a = input("Please tell us if you're going left or right here: ")
+if a == "left":
+  action = input("Do you swim or wait?")
+  if action == "swim":
+    print("You will find a hidden treasure.")
+  else:
+    print("You waited too long. The treasure is gone.")
+else:
+  print("Game over")
